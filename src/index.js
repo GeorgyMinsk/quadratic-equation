8904cac611b59fd196423e5a260ed68b0a970ebd
Georhi Buklaga <buklaga.98@yandex.ru>
2018-09-12 12:26:00 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+ function compareNumeric(x, y)
+ {
+     return x - y;
+}
+
 module.exports = function solveEquation(equation) {
   // your implementation
-}
+ var mass = equation.split (' ');
+ var arr = [];
+var a = (mass[0]);
+var b = (mass[3]+mass[4]);
+var c = (mass[7]+mass[8]);
+
+var D = (b*b-4*a*c);
+  if (D>=0) {
+  arr[0] = Math.round(((-b)-Math.sqrt(D))/(2*a));
+   arr[1] = Math.round(((-b)+Math.sqrt(D))/(2*a));
+	}
+	
+	else{
+		return null;
+	}
+
+ arr.sort(compareNumeric);
+     return arr;
+  }
+ 
+
