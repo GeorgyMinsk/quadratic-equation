25d7dfd6343353e9ce1efc4a5e2fef0839a72b27
Georgiy Buklaga <36643900+GeorgyMinsk@users.noreply.github.com>
2018-09-24 16:43:47 +0300

    Update index.js
    edd coment

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
- function compareNumeric(x, y)
+ function compare_numeric(x, y)
  {
      return x - y;
 }
 
 module.exports = function solveEquation(equation) {
-  // your implementation
+// разбиваем на массив
  var mass = equation.split (' ');
  var arr = [];
+// в соответстви с элементами массива привсаиваем коэфициентам a и b и c значения
 var a = (mass[0]);
 var b = (mass[3]+mass[4]);
 var c = (mass[7]+mass[8]);
-
+// вычесляем дескременант
 var D = (b*b-4*a*c);
   if (D>=0) {
+// 	  если он больше нуля вычисляем корни
   arr[0] = Math.round(((-b)-Math.sqrt(D))/(2*a));
    arr[1] = Math.round(((-b)+Math.sqrt(D))/(2*a));
 	}
-	
+// 	если меньше нуля корни комплексны и нам не интересно
 	else{
 		return null;
 	}
-
- arr.sort(compareNumeric);
+// сортируем массив как нам надо благодаря параметра sort - compare_numeric
+ arr.sort(compare_numeric);
      return arr;
   }
  
